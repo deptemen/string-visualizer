99b657efac90312391d41e2d496c4131681c557f
deptemen <50792495+deptemen@users.noreply.github.com>
2023-08-09 12:09:52 +0100

    added comments

--- string-visualizer-api/src/main/java/com/example/stringvisualizer/StringVisualizerApplication.java
+++ string-visualizer-api/src/main/java/com/example/stringvisualizer/StringVisualizerApplication.java
@@ -3,6 +3,8 @@ package com.example.stringvisualizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// String visualizer to compare two strings based on the max occurence of characters
+
 @SpringBootApplication
 public class StringVisualizerApplication {
 
